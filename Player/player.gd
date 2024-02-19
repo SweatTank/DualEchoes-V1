06b85b9fbcde907d137af71a623ff5924d399d3e
Shwetank Anand <77480447+SweatTank@users.noreply.github.com>
2024-02-19 12:11:34 +0530

    No Camera

--- Player/player.gd
+++ Player/player.gd
@@ -27,7 +27,7 @@ func _physics_process(delta):
 		if is_on_floor():
 			velocity.y = jump_velocity
 		elif not has_double_jumped:
-			velocity.y = double_jump_velocity
+			velocity.y = double_jump_velocity + jump_velocity
 			has_double_jumped = true
 			
 
