f077150cd7eb1552a3ad3c1b46d26a9ea4283164
Shwetank Anand <77480447+SweatTank@users.noreply.github.com>
2024-03-07 14:23:57 +0530

    enemy collision after death

--- skele/enemy.gd
+++ skele/enemy.gd
@@ -1,8 +1,6 @@
 extends CharacterBody2D
 
 
-#@export var speed : float = 200.0
-#@export var jump_velocity : float = -400.0
 @onready var animation_tree : AnimationTree = $AnimationTree
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -19,13 +17,5 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction = Input.get_axis("ui_left", "ui_right")
-	#if direction:
-		#velocity.x = direction * speed
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
