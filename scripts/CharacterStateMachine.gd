d7d8dc03f1c6c664841e1078125f971c92215509
Smile-002 <royance121@gmail.com>
2024-04-06 02:35:44 +0530

    menu full

--- scripts/CharacterStateMachine.gd
+++ scripts/CharacterStateMachine.gd
@@ -8,34 +8,34 @@ class_name CharacterStateMachine
 
 var states : Array[State]
 
-# Called when the node enters the scene tree for the first time.
+#Called when the node enters the scene tree for the first time.
 func _ready():
 	for child in get_children():
 		if(child is State):
 			states.append(child)
-			
+
 			child.character = character
 			child.playback = animation_tree["parameters/playback"]
-			
-			
+
+
 			child.connect("interrupt_state", on_state_interrupt_state)
-			
+
 		else:
 			push_warning("Child" + child.name + "is not a State for CharacterStateMachine")
 
 
 func check_if_can_move():
 	return current_state.can_move
-	
+
 func switch_states(new_state : State):
 	if (current_state != null):
 		current_state.on_exit()
 		current_state.next_state = null
-		
-		
+
+
 	current_state = new_state
 	current_state.on_enter()
-	
+
 
 func _input(event : InputEvent):
 	current_state.state_input(event)
@@ -43,12 +43,12 @@ func _input(event : InputEvent):
 func _physics_process(delta):
 	if(current_state.next_state != null):
 		switch_states(current_state.next_state)
-		
+
 	current_state.state_process(delta)
 
 func on_state_interrupt_state(new_state : State):
 	switch_states(new_state)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+#Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
